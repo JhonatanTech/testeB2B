6e2503a567a760b1e517e32775f8de9b7a2acb54
Jhonatan Carvalho <jhonatancarvalhotorres12@gmail.com>
2022-07-14 16:46:43 -0300

    'first'

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const { parallel } = require('gulp');
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
+const concat = require('gulp-concat')
+var csso = require('gulp-csso');
+
+function buildStyles() {
+    return gulp.src('css/scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('css/'))
+}
+exports.buildStyles = buildStyles
+
+function watch() {
+    gulp.watch('css/scss/*.scss', buildStyles)
+}
+exports.watch = watch
+
+exports.default = parallel(watch)
